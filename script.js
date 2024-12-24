2466e98e5325e1d93d0ed84d076f42b88d009fa9
abhi <108022705+Orbito7@users.noreply.github.com>
2024-12-23 18:44:47 -0800

    Added basic arithmatic operations

---
+++ script.js
@@ -0,0 +1,36 @@
+let number1;
+let number2;
+let operation;
+
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
+} 
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 == 0) {
+        alert("Cannot divide by 0");
+        return num1;
+    } else {
+        return num1 / num2;
+    }
+}
